c9b8c49c667e379fbc4ef81d9f15d63044a3749d
Artur Ventura <artur.ventura@gmail.com>
2009-09-28 11:04:18 +0100

    first commit

---
+++ double.py
@@ -0,0 +1,20 @@
+#! /usr/bin/python
+
+# -*- Mode: Python -*-
+# -*- coding: UTF-8 -*-
+# Copyright (C) 2009 by Artur Ventura
+#
+# File: double.py
+# Time-stamp: Tue Sep 15 15:02:00 2009
+#
+# Author: Artur Ventura
+#
+
+"""This is an example module.
+
+This is the docstring.
+"""
+
+def double(x):
+    "Return twice the argument"
+    return x * 2
